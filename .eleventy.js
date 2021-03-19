29574dc18e11853a9ec64c386b6d8bc00114d371
Joe Joiner <joejoinerr@gmail.com>
2021-03-19 23:26:28 +0000

    Add post list homepage

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+  // Allow top-level and page-level data to be combined
+  eleventyConfig.setDataDeepMerge(true);
+
   return {
     dir: {
       input: 'src',
