df2b78e1def09693aedc7f596c4228fe0bb92900
Joe Joiner <joejoinerr@gmail.com>
2021-03-19 23:51:53 +0000

    Fix date formatting by creating a new filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
+const { DateTime } = require('luxon')
+
 module.exports = function(eleventyConfig) {
   // Allow top-level and page-level data to be combined
   eleventyConfig.setDataDeepMerge(true);
+
+  // Create a date filter so that I can use format strings
+  eleventyConfig.addFilter('date', function(date, formatString) {
+    return DateTime.fromISO(date).toFormat(formatString);
+  });
 
   return {
     dir: {
