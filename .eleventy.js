12966aee9528058c03c753b79e6cc605a6374d27
Joe Joiner <joejoinerr@gmail.com>
2021-03-20 08:39:29 +0000

    Copy unsupported robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ module.exports = function(eleventyConfig) {
   // Allow top-level and page-level data to be combined
   eleventyConfig.setDataDeepMerge(true);
 
+  // Copy files not usually handled
+  eleventyConfig.addPassthroughCopy('src/robots.txt')
+
   // Create a date filter so that I can use format strings
   eleventyConfig.addFilter('date', function(date, formatString) {
     return DateTime.fromISO(date).toFormat(formatString);
